cab52d80822663b117d26fe05ab18e7bc2794df2
rinalim <46272802+rinalim@users.noreply.github.com>
2019-02-26 11:05:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+## install.sh
+# Install      :
+# cd /home/pi
+# git clone https://github.com/rinalim/VolumeJoy
+# cd VolumeJoy
+# chmod 755 install.sh
+# sudo ./install.sh
+#
+# Reference    :
+# https://github.com/RetroPie/RetroPie-Setup/blob/master/scriptmodules/supplementary/runcommand/joy2key.py
+# https://github.com/sana2dang/PauseMode
+
 sudo cp ./libraspidmx.so.1 /usr/lib
 
 mkdir /opt/retropie/configs/all/VolumeJoy/
@@ -6,6 +18,6 @@ cp -f -r ./VolumeJoy /opt/retropie/configs/all/
 sudo chmod 755 /opt/retropie/configs/all/VolumeJoy/pngvolume
 
 sudo sed -i '/volume.py/d' /opt/retropie/configs/all/runcommand-onstart.sh
-echo 'python /opt/retropie/configs/all/VolumeJoy/volume.py /dev/input/js0 &' >> /opt/retropie/configs/all/runcommand-onstart.sh
+echo 'python /opt/retropie/configs/all/VolumeJoy/volume.py /dev/input/js0 &' >> /opt/retropie/configs/all/autostart.sh
 
 echo 'Install Completed'
