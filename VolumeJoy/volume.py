ed036f209cb4063859b0f4ef6864ef9c6f815e9d
rinalim <ooll00@naver.com>
2020-08-11 02:18:13 +0900

    Update volume.py

--- VolumeJoy/volume.py
+++ VolumeJoy/volume.py
@@ -4,6 +4,7 @@ import os, sys, struct, time, fcntl, termios, signal
 import curses, errno, re
 from pyudev import Context
 from subprocess import *
+from datetime import datetime
 
 #    struct js_event {
 #        __u32 time;     /* event timestamp in milliseconds */
@@ -32,12 +33,21 @@ js_fds = []
 btn_up = -1
 btn_down = -1
 volume_step = [0,6,12,18,24,30,36,42,48,54,60,66,72,79,86,93,100]
+TIMEOUT = 2
+start_time = 0
 
 def run_cmd(cmd):
     # runs whatever in the cmd variable
     p = Popen(cmd, shell=True, stdout=PIPE)
     output = p.communicate()[0]
     return output
+
+def is_running(pname):
+    ps_grep = run_cmd("ps -ef | grep " + pname + " | grep -v grep")
+    if len(ps_grep) > 1 and "bash" not in ps_grep:
+        return True
+    else:
+        return False
 
 def kill_proc(name):
     ps_grep = run_cmd("ps -aux | grep " + name + "| grep -v 'grep'")
@@ -89,6 +99,7 @@ def read_event(fd):
             return event
 
 def process_event(event):
+    global start_time
 
     (js_time, js_value, js_type, js_number) = struct.unpack(event_format, event)
 
@@ -101,34 +112,35 @@ def process_event(event):
         #vol = int(run_cmd("amixer get PCM|grep -o [0-9]*%|sed 's/%//'"))
         #print vol
 
-        read_vol = int(run_cmd("amixer get PCM|grep -o [0-9]*%|sed 's/%//'"))
+        read_vol = int(run_cmd("amixer get HDMI|grep -o [0-9]*%|sed 's/%//'"))
         if js_number == btn_down:
-            if vol < 6:
+            if read_vol < 6:
                 vol = 0
             else:
-                vol = volume_step[vol/6-1]
+                vol = volume_step[read_vol/6-1]
             print "Decrease volume... " + str(vol)
         elif js_number == btn_up:
-            if vol > 93:
+            if read_vol > 93:
                 vol = 100
             else:
-                vol = volume_step[vol/6+1]
+                vol = volume_step[read_vol/6+1]
             print "Increase volume... " + str(vol)
         else:
             return False
  
         #vol = int(run_cmd("amixer get PCM|grep -o [0-9]*%|sed 's/%//'"))
-        run_cmd("amixer set PCM -- " + str(vol) + "%")
-        os.system("echo " + HOMEDIR + "png/volume" + str(vol/6) + ".png > /tmp/volume.txt")
+        run_cmd("amixer set HDMI -- " + str(vol) + "%")
+        start_time = datetime.now()
+        os.system("echo " + PATH_VOLUMEJOY + "png/volume" + str(vol/6) + ".png > /tmp/volume.txt")
         if is_running("omxiv-volume") == False:
             os.system(PATH_VOLUMEJOY + "omxiv-volume " + PATH_VOLUMEJOY + "png/background.png -l 30001 -a center &")
             os.system(PATH_VOLUMEJOY + "omxiv-volume /tmp/volume.txt -f -t 5 -T blend --duration 20 -l 30002 -a center &")
-    
+
     return True
 
 def main():
     
-    global btn_up, btn_down
+    global btn_up, btn_down, start_time
     
     if os.path.isfile(PATH_VOLUMEJOY + "button.cfg") == False:
         return False
@@ -174,6 +186,12 @@ def main():
             if cmp(js_devs, get_devices()):
                 close_fds(js_fds)
                 js_fds = []
+
+        if start_time != 0:
+            elapsed = (datetime.now() - start_time).total_seconds()
+            if elapsed >= TIMEOUT:
+                kill_proc("omxiv-volume")
+                start_time = 0
 
         if do_sleep:
             time.sleep(0.01)
